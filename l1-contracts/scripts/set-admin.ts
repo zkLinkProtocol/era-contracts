8e6f76fb8264e0d0b896426b1c7a3c004d540a6f
zkcarter <carter@zklink.org>
2024-02-06 19:06:33 +0800

    update set admin scripts

--- l1-contracts/scripts/set-admin.ts
+++ l1-contracts/scripts/set-admin.ts
@@ -11,40 +11,47 @@ async function main() {
 
   program.version("0.1.0").name("set-admin");
 
-  program.option("--gas-price <gas-price>").action(async (cmd) => {
-    const deployWallet = new Wallet(process.env.GOVERNOR_PRIVATE_KEY, provider);
-    console.log(`Using deployer wallet: ${deployWallet.address}`);
+  program
+    .option("--validator <validator>", "Validator address")
+    .option("--gas-price <gas-price>")
+    .action(async (cmd) => {
+      const deployWallet = new Wallet(process.env.GOVERNOR_PRIVATE_KEY, provider);
+      console.log(`Using deployer wallet: ${deployWallet.address}`);
 
-    const gasPrice = cmd.gasPrice ? parseUnits(cmd.gasPrice, "gwei") : await provider.getGasPrice();
-    console.log(`Using gas price: ${formatUnits(gasPrice, "gwei")} gwei`);
+      const gasPrice = cmd.gasPrice ? parseUnits(cmd.gasPrice, "gwei") : await provider.getGasPrice();
+      console.log(`Using gas price: ${formatUnits(gasPrice, "gwei")} gwei`);
 
-    const ownerAddress = deployWallet.address;
-    const validatorAddress = getAddressFromEnv("ETH_SENDER_SENDER_OPERATOR_COMMIT_ETH_ADDR");
+      const ownerAddress = deployWallet.address;
+      if (!cmd.validator) {
+        console.log("Validator address is not provided");
+        return;
+      }
+      const validatorAddress = cmd.validator;
 
-    const deployer = new Deployer({
-      deployWallet,
-      ownerAddress,
-      verbose: true,
+      const deployer = new Deployer({
+        deployWallet,
+        ownerAddress,
+        verbose: true,
+      });
+
+      const governance = deployer.governanceContract(deployWallet);
+      const zkSync = deployer.zkSyncContract(deployWallet);
+
+      const call = {
+        target: zkSync.address,
+        value: 0,
+        data: zkSync.interface.encodeFunctionData("setValidator", [validatorAddress, true]),
+      };
+
+      const operation = {
+        calls: [call],
+        predecessor: ethers.constants.HashZero,
+        salt: ethers.constants.HashZero,
+      };
+
+      await (await governance.scheduleTransparent(operation, 0)).wait();
+      await (await governance.execute(operation)).wait();
     });
-
-    const governance = deployer.governanceContract(deployWallet);
-    const zkSync = deployer.zkSyncContract(deployWallet);
-
-    const call = {
-      target: zkSync.address,
-      value: 0,
-      data: zkSync.interface.encodeFunctionData("setValidator", [validatorAddress, true]),
-    };
-
-    const operation = {
-      calls: [call],
-      predecessor: ethers.constants.HashZero,
-      salt: ethers.constants.HashZero,
-    };
-
-    await (await governance.scheduleTransparent(operation, 0)).wait();
-    await (await governance.execute(operation)).wait();
-  });
 
   await program.parseAsync(process.argv);
 }

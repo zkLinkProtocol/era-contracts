f34bb5f84813e41e968d7f78db109ae666dea5e2
zkbenny <benny@zklink.org>
2024-03-22 13:44:16 +0800

    fix: abdk-cvf-63

--- l1-contracts/contracts/zksync/libraries/PriorityQueue.sol
+++ l1-contracts/contracts/zksync/libraries/PriorityQueue.sol
@@ -62,9 +62,10 @@ library PriorityQueue {
 
     /// @return The unprocessed priority operation at the index `i` from the queue
     function index(Queue storage _queue, uint256 i) internal view returns (PriorityOperation memory) {
-        require(_queue.head + i < _queue.tail, "D"); // invalid index
+        uint256 index = _queue.head + i;
+        require(index < _queue.tail, "D"); // invalid index
 
-        return _queue.data[_queue.head + i];
+        return _queue.data[index];
     }
 
     /// @return The first unprocessed priority operation from the queue
